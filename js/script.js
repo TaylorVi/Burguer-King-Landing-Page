9be4d550f49a56eee7e3d1d5de8ecb9ef8391f8c
Taylor <taylor.vieira.77@gmail.com>
2023-11-01 13:44:46 -0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 120,
+    speed: 800,
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.btn-prev-right',
+        prevEl: '.btn-prev-left',
+      },
+    
+  });
